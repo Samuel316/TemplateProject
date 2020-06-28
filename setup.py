481574751c94b7bdbaf8ae348f43b4d4f60b6a24
Samuel Lloyd <samuel.lloyd@ed.ac.uk>
2020-06-28 17:12:18 +0100

    Template Project

--- setup.py
+++ setup.py
@@ -4,13 +4,25 @@
 Copyright Samuel Lloyd
 28/06/2020
 samueljohnlloyd12@gmail.com
-
-Parameters
-----------
-
-Return
-------
 """
 
-if __name__ == '__main__':
-    pass
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="templateproject",
+    version="0.1",
+    author="Samuel Lloyd",
+    author_email="samueljohnlloyd12@gmail.com",
+    description="",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">3.7",
+)
